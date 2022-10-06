8e15ba0a20cefe435f34f20925fc51b56a7602df
madhuri-15 <madhuri15893@gmail.com>
2022-10-06 14:08:31 +0530

    Added a flask web services
    Update

--- Week5-Model deployment/predict.py
+++ Week5-Model deployment/predict.py
@@ -1,39 +1,40 @@
 
 # Load the model
 import pickle
+from flask import Flask
+from flask import request
+from flask import jsonify
 
 
-# open pickle file in namespace
+# open pickle file
 input_file = "model_C=1.0.bin"
 with open(input_file, 'rb') as f_in:
     dv, model = pickle.load(f_in)
 
-customer = {
-    'gender':'female',
-    'seniorcitizen':0,
-    'partern':'yes',
-    'dependents':'no', 
-    'phoneservice':'no',
-    'multiplelines':'no_phone_service', 
-    'internetservice':'dsl',
-    'onlinesecurity':'no',
-    'onlinebackup':'yes',
-    'deviceprotection':'no',
-    'techsupport':'no',
-    'streamingtv':'no',
-    'streamingmovies':'no',
-    'contract':'month-to-month',
-    'paperlessbilling':'yes',
-    'paymentmethod':'electronic_check',
-    'tenure':1,
-    'monthlycharges':29.85,
-    'totalcharges':29.85
-}
 
-X = dv.transform([customer])
-output = model.predict_proba(X)[0, 1]
+# prediction function for single input value
+def prediction(customer):
+    X = dv.transform([customer])
+    y_preds = model.predict_proba(X)[0, 1]
+    churn = y_preds >= 0.5
+    return churn, y_preds
+    
 
-print("Input::", customer)
-print(f"Will customer churn? - {'Y' if output > 0.5 else 'N'}")
+app = Flask("churn")
+
+@app.route("/predict", methods=['POST'])
+def predict():
+    
+    customer = request.get_json()
+    churn, y_predicts = prediction(customer)
+    
+    result = {
+        'churn_probability': float(y_predicts),
+        'churn': bool(churn)
+        }
+    
+    return jsonify(result)
 
 
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port="9696")
