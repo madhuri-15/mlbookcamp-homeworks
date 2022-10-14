a43b29a62cdfcec4f058e7f92c7c82af2545ba51
Madhuri Patil <60868865+madhuri-15@users.noreply.github.com>
2022-10-14 15:19:08 +0530

    Added python files

--- Week5-Model deployment/Homework-#5/client_test.py
+++ Week5-Model deployment/Homework-#5/client_test.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import requests
 
 url = "http://localhost:9696/predict"
@@ -10,17 +9,3 @@ print(result.json())
 
 
 
-=======
-import requests
-
-url = "http://localhost:9696/predict"
-client = {"reports": 0, "share": 0.245, "expenditure": 3.438, "owner": "yes"}
-client = {"reports": 0, "share": 0.245, "expenditure": 3.438, "owner": "yes"}
-result = requests.post(url, json=client)
-print(result.json())
-    
-
-
-
-
->>>>>>> 7944fcab67325e7a4ed6c82e679e7873d763197e
